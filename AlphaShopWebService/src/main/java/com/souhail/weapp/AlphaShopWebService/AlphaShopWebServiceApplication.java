8d39f41faa30ab8ca969d78d537f9c8db627ad89
D3moniako <soleroboo@gmail.com>
2022-06-20 16:42:37 +0200

    aggiunta backend

---
+++ AlphaShopWebService/src/main/java/com/souhail/weapp/AlphaShopWebService/AlphaShopWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.souhail.weapp.AlphaShopWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaShopWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaShopWebServiceApplication.class, args);
+	}
+
+}
---
+++ AlphaShopWebService/src/test/java/com/souhail/weapp/AlphaShopWebService/AlphaShopWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souhail.weapp.AlphaShopWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaShopWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
